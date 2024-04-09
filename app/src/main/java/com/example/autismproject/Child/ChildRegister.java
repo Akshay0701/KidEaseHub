3caa9f7f766314df86954c16d4c5ee3f1ddab658
Akshay Vilas Jadhav <52353967+Akshay0701@users.noreply.github.com>
2024-04-09 19:23:52 -0400

    Created UI pages for Parent and Child
    Ui pages include login register for parent and child and home page

---
+++ app/src/main/java/com/example/autismproject/Child/ChildRegister.java
@@ -0,0 +1,16 @@
+package com.example.autismproject.Child;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.autismproject.R;
+
+public class ChildRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child_register);
+    }
+}
---
+++ app/src/main/java/com/example/autismproject/Parent/ParentLogin.java
@@ -0,0 +1,30 @@
+package com.example.autismproject.Parent;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.autismproject.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ParentLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parent_login);
+    }
+}
