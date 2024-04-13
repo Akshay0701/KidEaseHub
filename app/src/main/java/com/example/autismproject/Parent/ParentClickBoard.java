ee589edbe4968a92f9a4cf98da8684d1825d29d2
Akshay Vilas Jadhav <52353967+Akshay0701@users.noreply.github.com>
2024-04-12 20:16:07 -0400

    added assets image and created base parent clickboard UI

---
+++ app/src/main/java/com/example/autismproject/Parent/ParentClickBoard.java
@@ -0,0 +1,16 @@
+package com.example.autismproject.Parent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.autismproject.R;
+
+public class ParentClickBoard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parent_click_board);
+    }
+}
