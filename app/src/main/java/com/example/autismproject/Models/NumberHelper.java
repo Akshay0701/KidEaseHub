5dd555d3b945c7703720e02acbbf4e5caf57e918
Akshay Vilas Jadhav <52353967+Akshay0701@users.noreply.github.com>
2024-04-23 13:11:29 -0400

    Number screen completed

---
+++ app/src/main/java/com/example/autismproject/Models/NumberHelper.java
@@ -0,0 +1,17 @@
+package com.example.autismproject.Models;
+
+public class NumberHelper {
+
+    String title;
+
+    public NumberHelper(String title) {
+        this.title = title;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+}
+
+
