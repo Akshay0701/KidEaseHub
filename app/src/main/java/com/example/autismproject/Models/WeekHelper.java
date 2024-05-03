01c4362402a03db588189dddf3adb4d56d8a97da
Akshay Vilas Jadhav <52353967+Akshay0701@users.noreply.github.com>
2024-05-03 13:31:19 -0400

    Reapply "Unit Test for Alphabet and Week Acitvity"
    This reverts commit 08365b1dcb858a7087007d6a2c26c05375b3520c.

---
+++ app/src/main/java/com/example/autismproject/Models/WeekHelper.java
@@ -0,0 +1,17 @@
+package com.example.autismproject.Models;
+
+public class WeekHelper {
+
+    String title;
+
+    public WeekHelper(String title) {
+        this.title = title;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+}
+
+
